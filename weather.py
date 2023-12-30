11f638c6ddaa21e006713dcca73356bcb1efbf53
Oleks Chemerys <oleks@quick.cz>
2023-12-30 15:55:51 -0400

    refactor weather module

--- weather.py
+++ weather.py
@@ -1,4 +1,6 @@
+import sys
 import requests
+
 
 def get_forecast(url):
   forecast_data = []
@@ -19,3 +21,20 @@ def get_forecast(url):
 
   return forecast_data
 
+
+def main(api_url, api_key, region, forecast_days):
+  url = f'{api_url}?key={api_key}&q={region}&days={forecast_days}'
+  fd = get_forecast(url)
+  for dw in fd:
+    print(dw)
+
+
+if __name__ == '__main__':
+  # for idx, arg in enumerate(sys.argv):
+  #   print("Argument #{} is {}".format(idx, arg))
+  # print ("No. of arguments passed is ", len(sys.argv))
+  url = sys.argv[1]
+  key = sys.argv[2]
+  reg = sys.argv[3]
+  days = sys.argv[4]
+  main(url, key, reg, days)
